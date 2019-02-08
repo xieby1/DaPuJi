275e2dd0fd93826b1a59f1e3fbe37ca2946e9f91
XieBenyi <xieby1@outlook.com>
2019-02-08 16:30:25 +0800

    减少用于支持键盘鼠标手柄的冗余代码，reduce redundant keyboard/mouse/controller code

--- performance.js
+++ performance.js
@@ -10,80 +10,46 @@ function connecthandler(e) {
 function disconnecthandler(e) {
     delete controllers[e.gamepad.index];
 }
-
-const controllerPress = {
-    LT: pressLow,
-    RT: pressHigh,
-    LB: pressFlat,
-    RB: pressSharp,
-    L:  pressDo,
-    U:  pressRe,
-    R:  pressMi,
-    D:  pressFa,
-    X:  pressSo,
-    Y:  pressLa,
-    B:  pressSi,
-    A:  pressDD
+const ControllerTemplate = {
+    LT: 'low', LB: 'flat', RB: 'sharp', RT: 'high',
+    L: 'do', U: 're', R: 'mi', D: 'fa',
+    X: 'so', Y: 'la', B: 'si', A: 'dd'
 };
-const controllerRelease = {
-    LT: releaseLow,
-    RT: releaseHigh,
-    LB: releaseFlat,
-    RB: releaseSharp,
-    L:  releaseDo,
-    U:  releaseRe,
-    R:  releaseMi,
-    D:  releaseFa,
-    X:  releaseSo,
-    Y:  releaseLa,
-    B:  releaseSi,
-    A:  releaseDD
-};
+const ControllerPress = {};
+const ControllerRelease = {};
+for(let key in ControllerTemplate)
+{
+    ControllerPress[key] = FuncPress[ControllerTemplate[key]];
+    ControllerRelease[key] = FuncRelease[ControllerTemplate[key]];
+}
 
-// 上一次刷新各个按键的状态。true：按下状态，false：松开状态
-// let LT=false, RT=false, LB=false, RB=false;
-let LT=false, RT=false, LB=false, RB=false;
-let L=false, U=false, R=false, D=false;
-let X=false, Y=false, B=false, A=false;
+const ControllerPreFlags = {}; // 上一次刷新各个按键的状态。true：按下状态，false：松开状态
+const ControllerCurFlags = {}; // 本次刷新时各个按键的状态。
+for(let key in ControllerTemplate)
+    ControllerPreFlags[key] = false;
+const ControllerButtonNum = {
+    LT: 6, LB: 4, RB: 5, RT: 7,
+    L: 14, U: 12, R: 15, D: 13,
+    X: 2, Y: 3, B: 1, A: 0
+};
 function updateStatus() {
     if (!haveEvents) {
         scangamepads();
     }
 
     for (let j in controllers) {
-        var controller = controllers[j];
-        let bs = controller.buttons;
-        // 当前各个按键的状态
-        let cLT=bs[6].pressed, cRT=bs[7].pressed, cLB=bs[4].pressed, cRB=bs[5].pressed;
-        let cL=bs[14].pressed, cU=bs[12].pressed, cR=bs[15].pressed, cD=bs[13].pressed;
-        let cX=bs[2].pressed, cY=bs[3].pressed, cB=bs[1].pressed, cA=bs[0].pressed;
-
-        if(cLT && !LT) controllerPress.LT();
-        if(cRT && !RT) controllerPress.RT();
-        if(cLB && !LB) controllerPress.LB();
-        if(cRB && !RB) controllerPress.RB();
-        if(cL && !L) controllerPress.L();
-        if(cU && !U) controllerPress.U();
-        if(cR && !R) controllerPress.R();
-        if(cD && !D) controllerPress.D();
-        if(cX && !X) controllerPress.X();
-        if(cY && !Y) controllerPress.Y();
-        if(cB && !B) controllerPress.B();
-        if(cA && !A) controllerPress.A();
-        if(!cLT && LT) controllerRelease.LT();
-        if(!cRT && RT) controllerRelease.RT();
-        if(!cLB && LB) controllerRelease.LB();
-        if(!cRB && RB) controllerRelease.RB();
-        if(!cL && L) controllerRelease.L();
-        if(!cU && U) controllerRelease.U();
-        if(!cR && R) controllerRelease.R();
-        if(!cD && D) controllerRelease.D();
-        if(!cX && X) controllerRelease.X();
-        if(!cY && Y) controllerRelease.Y();
-        if(!cB && B) controllerRelease.B();
-        if(!cA && A) controllerRelease.A();
-
-        LT=cLT; RT=cRT; LB=cLB; RB=cRB; L=cL; U=cU; R=cR; D=cD; X=cX; Y=cY; B=cB; A=cA;
+        let controller = controllers[j];
+        for(let key in ControllerTemplate) // 当前各个按键的状态
+            ControllerCurFlags[key] = controller.buttons[ ControllerButtonNum[key] ].pressed;
+        for(let key in ControllerTemplate)
+        {
+            if(ControllerCurFlags[key] && !ControllerPreFlags[key])
+                ControllerPress[key]();
+            if(!ControllerCurFlags[key] && ControllerPreFlags[key])
+                ControllerRelease[key]();
+        }
+        for(let key in ControllerTemplate) // 本次按键状态变成上一次按键状态
+            ControllerPreFlags[key] = ControllerCurFlags[key];
     }
 
     requestAnimationFrame(updateStatus);
@@ -110,48 +76,20 @@ if (!haveEvents) {
 }
 // 以上为手柄相关
 // 以下为键盘相关
-const keyboardAction = {
-    low: 'ShiftLeft',
-    flat: 'AltLeft',
-    do: 'KeyA',
-    re: 'KeyS',
-    mi: 'KeyD',
-    fa: 'KeyF',
-    so: 'KeyJ',
-    la: 'KeyK',
-    si: 'KeyL',
-    dd: 'Semicolon',
-    sharp: 'AltRight',
-    high: 'ShiftRight'
+const KeyboardAction = {
+    low: 'ShiftLeft', flat: 'AltLeft', sharp: 'AltRight', high: 'ShiftRight',
+    do: 'KeyA', re: 'KeyS', mi: 'KeyD', fa: 'KeyF',
+    so: 'KeyJ', la: 'KeyK', si: 'KeyL', dd: 'Semicolon'
 };
 document.addEventListener('keydown', (e)=>{
-    switch (e.code) {
-        case keyboardAction.low: pressLow(); break;
-        case keyboardAction.flat: pressFlat(); break;
-        case keyboardAction.do: pressDo(); break;
-        case keyboardAction.re: pressRe(); break;
-        case keyboardAction.mi: pressMi(); break;
-        case keyboardAction.fa: pressFa(); break;
-        case keyboardAction.so: pressSo(); break;
-        case keyboardAction.la: pressLa(); break;
-        case keyboardAction.si: pressSi(); break;
-        case keyboardAction.dd: pressDD(); break;
-        default:
-    }
+    for(let key in KeyboardAction)
+        if(e.code === KeyboardAction[key])
+            FuncPress[key]();
 });
 document.addEventListener('keyup', (e)=>{
-    switch (e.code) {
-        case keyboardAction.low: releaseLow(); break;
-        case keyboardAction.flat: releaseFlat(); break;
-        case keyboardAction.do: releaseDo(); break;
-        case keyboardAction.re: releaseRe(); break;
-        case keyboardAction.mi: releaseMi(); break;
-        case keyboardAction.fa: releaseFa(); break;
-        case keyboardAction.so: releaseSo(); break;
-        case keyboardAction.la: releaseLa(); break;
-        case keyboardAction.si: releaseSi(); break;
-        case keyboardAction.dd: releaseDD(); break;
-        default:
-    }
+
+    for(let key in KeyboardAction)
+        if(e.code === KeyboardAction[key])
+            FuncRelease[key]();
 });
 // 以上为键盘相关
--- src/functions/keyboard.js
+++ src/functions/keyboard.js
@@ -1,175 +1,60 @@
 let player;
 const audioContext = new AudioContext();
 Soundfont.instrument(audioContext, 'acoustic_grand_piano').then(function (p) {player=p;});
+const KeyTemplate = {
+    low: null, flat: null, sharp: null, high: null,
+    do: 0, re: 2, mi: 4, fa: 5, so: 7, la: 9, si: 11, dd: 12};
 
+// 标志表示对应的琴键是否被按下
+const KeyFlags = {};
+for(let key in KeyTemplate)
+    KeyFlags[key] = false;
 // 各个琴键在窗口里对应的对象(DOM)
-const lowKey    = document.getElementById('low');
-const flatKey   = document.getElementById('flat');
-const doKey     = document.getElementById('do');
-const reKey     = document.getElementById('re');
-const miKey     = document.getElementById('mi');
-const faKey     = document.getElementById('fa');
-const soKey     = document.getElementById('so');
-const laKey     = document.getElementById('la');
-const siKey     = document.getElementById('si');
-const ddKey     = document.getElementById('dd');
-const sharpKey  = document.getElementById('sharp');
-const highKey   = document.getElementById('high');
-const SIDEKEY = 'sideKey';
-const PRESS = ' press';
-const MAINKEY = 'mainKey';
-
-// counters
-// 数字表示有几个地方在按它，可能来源鼠标、键盘和手臂
-let cLow=0, cFlat=0, cSharp=0, cHigh=0;
-let cDo=0, cRe=0, cMi=0, cFa=0, cSo=0, cLa=0, cSi=0, cDD=0;
+const Keys = {}; const SIDEKEY = 'sideKey'; const PRESS = ' press'; const MAINKEY = 'mainKey';
+for(let key in KeyTemplate)
+    Keys[key] = document.getElementById(key);
 let do1 = 60; // do对应的midi-number
 let quiteMode = false; // true：松开按键时音会停掉（笛子小提琴等），false：松开按键时音会一直延续（钢琴等）
 // const performable = new Array(38); // 1+12+12+12+1
-function getMIDINum() {
-    let midi = do1;
-    if(cLow>0) midi -= 12;
-    if(cHigh>0) midi += 12;
-    if(cFlat>0) midi--;
-    if(cSharp>0) midi++;
-    return midi;
+const FuncPress = {}; // 对应按键被按下时的动作
+const FuncRelease = {}; // 对应按键被松开时的动作
+for(let key in KeyTemplate)
+{
+    if(key==='low' || key==='flat' || key==='sharp' || key==='high')
+    {
+        FuncPress[key] = ()=>{
+            Keys[key].className = SIDEKEY+PRESS;
+            KeyFlags[key] = true;
+        };
+        FuncRelease[key] = ()=>{
+            Keys[key].className = SIDEKEY;
+            KeyFlags[key] = false;
+        };
+    }
+    else
+    {
+        FuncPress[key] = ()=>{
+            let midi = do1;
+            if(KeyFlags['low']) midi -= 12;
+            if(KeyFlags['high']) midi += 12;
+            if(KeyFlags['flat']) midi--;
+            if(KeyFlags['sharp']) midi++;
+            midi += KeyTemplate[key];
+            if(!KeyFlags[key])
+                player.play(midi);
+            Keys[key].className = MAINKEY+PRESS;
+            KeyFlags[key] = true;
+        };
+        FuncRelease[key] = ()=>{
+            Keys[key].className = MAINKEY;
+            KeyFlags[key] = false;
+        };
+    }
 }
 
-function pressLow() { if(cLow===0)lowKey.className=SIDEKEY+PRESS; cLow++;}
-function pressFlat() { if(cFlat===0)flatKey.className=SIDEKEY+PRESS; cFlat++;}
-function pressSharp() { if(cSharp===0)sharpKey.className=SIDEKEY+PRESS; cSharp++;}
-function pressHigh() { if(cHigh===0)highKey.className=SIDEKEY+PRESS; cHigh++;}
-function pressDo() {
-    if(cDo===0) {
-        player.play(getMIDINum());
-        doKey.className = MAINKEY+PRESS;
-    }
-    cDo++;
-}
-function pressRe() {
-    if(cRe===0) {
-        player.play(getMIDINum()+2);
-        reKey.className = MAINKEY+PRESS;
-    }
-    cRe++;
-}
-function pressMi() {
-    if(cMi===0) {
-        player.play(getMIDINum()+4);
-        miKey.className = MAINKEY+PRESS;
-    }
-    cMi++;
-}
-function pressFa() {
-    if(cFa===0) {
-        player.play(getMIDINum()+5);
-        faKey.className = MAINKEY+PRESS;
-    }
-    cFa++;
-}
-function pressSo() {
-    if(cSo===0) {
-        player.play(getMIDINum()+7);
-        soKey.className = MAINKEY+PRESS;
-    }
-    cSo++;
-}
-function pressLa() {
-    if(cLa===0) {
-        player.play(getMIDINum()+9);
-        laKey.className = MAINKEY+PRESS;
-    }
-    cLa++;
-}
-function pressSi() {
-    if(cSi===0) {
-        player.play(getMIDINum()+11);
-        siKey.className = MAINKEY+PRESS;
-    }
-    cSi++;
-}
-function pressDD() {
-    if(cDD===0) {
-        player.play(getMIDINum()+12);
-        ddKey.className = MAINKEY+PRESS;
-    }
-    cDD++;
-}
-function releaseLow() {cLow--; if(cLow===0)lowKey.className=SIDEKEY;}
-function releaseFlat() {cFlat--; if(cFlat===0)flatKey.className=SIDEKEY;}
-function releaseSharp() {cSharp--; if(cSharp===0)sharpKey.className=SIDEKEY;}
-function releaseHigh() {cHigh--; if(cHigh===0)highKey.className=SIDEKEY;}
-function releaseDo() {
-    cDo--;
-    if(cDo===0) {
-        doKey.className = MAINKEY;
-    }
-}
-function releaseRe() {
-    cRe--;
-    if(cRe===0) {
-        reKey.className = MAINKEY;
-    }
-}
-function releaseMi() {
-    cMi--;
-    if(cMi===0) {
-        miKey.className = MAINKEY;
-    }
-}
-function releaseFa() {
-    cFa--;
-    if(cFa===0) {
-        faKey.className = MAINKEY;
-    }
-}
-function releaseSo() {
-    cSo--;
-    if(cSo===0) {
-        soKey.className = MAINKEY;
-    }
-}
-function releaseLa() {
-    cLa--;
-    if(cLa===0) {
-        laKey.className = MAINKEY;
-    }
-}
-function releaseSi() {
-    cSi--;
-    if(cSi===0) {
-        siKey.className = MAINKEY;
-    }
-}
-function releaseDD() {
-    cDD--;
-    if(cDD===0) {
-        ddKey.className = MAINKEY;
-    }
-}
 // 以下鼠标相关
-lowKey.onmousedown = pressLow;
-flatKey.onmousedown = pressFlat;
-doKey.onmousedown = pressDo;
-reKey.onmousedown = pressRe;
-miKey.onmousedown = pressMi;
-faKey.onmousedown = pressFa;
-soKey.onmousedown = pressSo;
-laKey.onmousedown = pressLa;
-siKey.onmousedown = pressSi;
-ddKey.onmousedown = pressDD;
-sharpKey.onmousedown = pressSharp;
-highKey.onmousedown = pressHigh;
-lowKey.onmouseup = releaseLow;
-flatKey.onmouseup = releaseFlat;
-doKey.onmouseup = releaseDo;
-reKey.onmouseup = releaseRe;
-miKey.onmouseup = releaseMi;
-faKey.onmouseup = releaseFa;
-soKey.onmouseup = releaseSo;
-laKey.onmouseup = releaseLa;
-siKey.onmouseup = releaseSi;
-ddKey.onmouseup = releaseDD;
-sharpKey.onmouseup = releaseSharp;
-highKey.onmouseup = releaseHigh;
-// 以上为键盘相关
+for(let key in KeyTemplate)
+{
+    Keys[key].onmousedown = FuncPress[key];
+    Keys[key].onmouseup = FuncRelease[key];
+}
